d719f3b78e73d9f0d3f3867f97010ce05a1ffbbc
DivyaAdabala07 <110604521+DivyaAdabala07@users.noreply.github.com>
2024-05-05 14:56:29 +0530

    Create Contact.js

---
+++ food/src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center mt-5">Contact Us</h1>
+      <p className="lead text-center mt-3">
+        Have any questions or feedback? Contact us at [contact@budgeat.com](mailto:contact@budgeat.com)
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
