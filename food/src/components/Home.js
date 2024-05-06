d15933c441916b7108e70df9abd32d657f7ebf11
marthagrace <110624519+marthagrace@users.noreply.github.com>
2024-05-06 11:37:00 +0530

    Update Home.js

--- food/src/components/Home.js
+++ food/src/components/Home.js
@@ -4,16 +4,22 @@ import './HomePage.css';
 function HomePage() {
   return (
     <div className="home-container">
-      <h1 className="text-center mt-5 home-title">BudgEAT</h1>
-      <p className="lead text-center mt-3 home-subtitle">
-        Where Flavour meets Affordability
-      </p>
-      <div className="text-center mt-5">
-        <button className="btn btn-primary get-started-btn">
-          <a href="/getstarted" className="text-white">
-            Get Started
-          </a>
-        </button>
+      {/* Image container */}
+      {/* Text content container */}
+      <div className='body'>
+        <h1 className="text-center mt-5 home-title"><span className='budg'>Budg</span><span className='eat'>EAT</span></h1>
+        <p className="lead text-center mt-3 home-subtitle">
+          Where Flavour meets Affordability
+        </p>
+        <div className="text-center mt-5">
+          <div className='btn'>
+            <button className="btn btn-primary get-started-btn">
+              <a href="/getstarted" className="text-white">
+                Get Started
+              </a>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
