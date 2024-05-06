d15cc9d62676152f2ee584915850f771ba81e12d
marthagrace <110624519+marthagrace@users.noreply.github.com>
2024-05-06 11:38:14 +0530

    Update About.js

--- food/src/components/About.js
+++ food/src/components/About.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
   return (
     <div className="container">
       <h1 className="text-center mt-5">About Us</h1>
-      <p className="lead text-center mt-3">
+      <center><h1 className='title'>BudgEAT</h1></center>
+      <p className="one">
         BudgEAT is a food recommendation system that helps you find delicious and affordable food options based on your budget. Our mission is to make great food accessible to everyone.
       </p>
+      <div className='two'>
+      </div>
     </div>
   );
 };
